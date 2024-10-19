4c351fa9791f804bedc234ad63e768f2c806e649
Ohnoo1234 <daotung1771@gmail.com>
2024-10-20 00:11:40 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mẹ Lượt';
+const giftUrl = '';
+const eventName = '20/10';
+const titleCard = 'Gửi Tặng Mẹ Lượt';
+const contentCard = 'Chúc mẹ có một ngày 20/10 tràn ngập niềm vui và sức khoẻ dồi dào và đặc biệt luôn luôn trẻ đẹp như những bông hoa ạ' ;
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
